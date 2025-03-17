58c32348c240499c5e92341209822f950ec1a8bb
Park YongJune <redplus@redplus.net>
2025-03-17 18:30:36 +0900

    Expression-bodied method

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -2,11 +2,8 @@
 
 await ExecuteQueryAsync(array);
 
-async Task ExecuteQueryAsync(int[] array, bool printMessage = true)
-{
-    var query = array.AsQueryable().Where(i => i > 5 && i % 2 == 0);
-
-    if (printMessage)
-        foreach (int value in query)
-            await Task.Run(() => WriteLine(value));
-}
+// C# 6.0 기능: Expression-bodied method 적용
+async Task ExecuteQueryAsync(int[] array, bool printMessage = true) =>
+    await (printMessage ? Task.WhenAll(array.AsQueryable()
+        .Where(i => i > 5 && i % 2 == 0)
+        .Select(i => Task.Run(() => WriteLine(i)))) : Task.CompletedTask);
