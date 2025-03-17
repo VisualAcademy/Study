6b3127d561153e40460a18d48797d4511a3f6b93
Park YongJune <redplus@redplus.net>
2025-03-17 17:10:52 +0900

    Generic

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -1,14 +1,16 @@
 ﻿int[] array = { 3, 9, 5, 13, 12, 30 };
-IEnumerable<int> query = Filter(array, GreaterThanFive);
+int[] query = Filter(array, GreaterThanFive);
 foreach (int value in query) { WriteLine(value); }
 
 bool GreaterThanFive(int i) { return i > 5; }
 
-IEnumerable<int> Filter(IEnumerable<int> src, Predicate p)
+int[] Filter(int[] src, Predicate p)
 {
     List<int> dst = new List<int>();
     foreach (int value in src) { if (p(value)) dst.Add(value); }
-    return dst.ToArray();
+    int[] result = new int[dst.Count];
+    for (int i = 0; i < result.Length; i++) { result[i] = dst[i]; }
+    return result;
 }
 
 delegate bool Predicate(int i);
