dd56e228fbc8d707bac6c2e4dd3044120e6e190d
Park YongJune <redplus@redplus.net>
2025-03-17 18:09:39 +0900

    C# Linq

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -1,11 +1,3 @@
 ﻿int[] array = { 3, 9, 5, 13, 12, 30 };
 var query = array.AsQueryable().Where(i => i > 5).Where(i => i % 2 == 0);
 foreach (int value in query) { WriteLine(value); }
-
-static class MyExtensions
-{
-    public static IEnumerable<T> Where<T>(this IEnumerable<T> src, Predicate<T> p)
-    {
-        foreach (T value in src) { if (p(value)) yield return value; }
-    }
-}
