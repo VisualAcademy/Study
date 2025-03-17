9c9123b59f5dd8d3a7c523b765164b2ff59ad2c5
Park YongJune <redplus@redplus.net>
2025-03-17 17:55:40 +0900

    C# Extension Method

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -1,8 +1,11 @@
 ﻿int[] array = { 3, 9, 5, 13, 12, 30 };
-var query = Filter(Filter(array, i => i > 5), i => i % 2 == 0); // pipeline
+var query = array.Filter(i => i > 5).Filter(i => i % 2 == 0);
 foreach (int value in query) { WriteLine(value); }
 
-IEnumerable<T> Filter<T>(IEnumerable<T> src, Predicate<T> p)
+static class MyExtensions
 {
-    foreach (T value in src) { if (p(value)) yield return value; } 
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> src, Predicate<T> p)
+    {
+        foreach (T value in src) { if (p(value)) yield return value; }
+    }
 }
