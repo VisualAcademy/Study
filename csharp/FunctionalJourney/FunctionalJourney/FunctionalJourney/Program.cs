a4f76735677c4d837797fcc75ed0a6309be0fb5e
Park YongJune <redplus@redplus.net>
2025-03-17 18:08:20 +0900

    AsQueryable

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -1,10 +1,10 @@
 ﻿int[] array = { 3, 9, 5, 13, 12, 30 };
-var query = array.Filter(i => i > 5).Filter(i => i % 2 == 0);
+var query = array.AsQueryable().Where(i => i > 5).Where(i => i % 2 == 0);
 foreach (int value in query) { WriteLine(value); }
 
 static class MyExtensions
 {
-    public static IEnumerable<T> Filter<T>(this IEnumerable<T> src, Predicate<T> p)
+    public static IEnumerable<T> Where<T>(this IEnumerable<T> src, Predicate<T> p)
     {
         foreach (T value in src) { if (p(value)) yield return value; }
     }
