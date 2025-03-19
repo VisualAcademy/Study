9686cf2a99bf122e2541b658c1e16d9d08ff6b77
Park YongJune <redplus@redplus.net>
2025-03-19 11:31:39 +0900

    C# 3.0 Lambda Expressions

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -1,10 +1,11 @@
-﻿int[] array = { 3, 9, 5, 13, 12, 30 };
+﻿// C# 3.0 Lambda Expressions
+int[] array = { 3, 9, 5, 13, 12, 30 };
 var query = Filter(array, i => i > 5);
 foreach (int value in query) { WriteLine(value); }
 
 IEnumerable<T> Filter<T>(IEnumerable<T> src, Predicate<T> p)
 {
-    foreach (T value in src) { if (p(value)) yield return value; } // C# 2.0 yield
+    foreach (T value in src) { if (p(value)) yield return value; }
 }
 
 delegate bool Predicate<T>(T i);
