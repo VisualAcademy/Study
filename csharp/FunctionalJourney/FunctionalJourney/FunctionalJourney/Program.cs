685f8fd772ed673daad55755bd96148cd86485b1
Park YongJune <redplus@redplus.net>
2025-03-19 11:31:05 +0900

    C# 2.0 무명 메서드(Anonymous Method)

--- csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
+++ csharp/FunctionalJourney/FunctionalJourney/FunctionalJourney/Program.cs
@@ -1,10 +1,11 @@
-﻿int[] array = { 3, 9, 5, 13, 12, 30 };
-var query = Filter(array, delegate (int i) { return i > 5; });
+﻿// 무명 메서드(Anonymous Method)
+int[] array = { 3, 9, 5, 13, 12, 30 };
+IEnumerable<int> query = Filter(array, delegate (int i) { return i > 5; });
 foreach (int value in query) { WriteLine(value); }
 
 IEnumerable<T> Filter<T>(IEnumerable<T> src, Predicate<T> p)
 {
-    foreach (T value in src) { if (p(value)) yield return value; } // C# 2.0 yield
+    foreach (T value in src) { if (p(value)) yield return value; }
 }
 
 delegate bool Predicate<T>(T i);
